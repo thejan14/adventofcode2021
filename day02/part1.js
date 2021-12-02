3eb40ed91d4a7ae95f45ec490f09f6ba6b208518
thejan14 <jan.oberpichler@googlemail.com>
2021-12-02 18:04:53 +0100

    solution day 1 part 1

---
+++ day02/part1.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: fs.createReadStream(path.join(__dirname, "input.txt")),
+    crlfDelay: Infinity
+});
+
+let depth = 0;
+let horizontalPos = 0;
+rl.on("line", (line) => {
+    const value = Number(line.charAt(line.length - 1));
+    const direction = line.charAt(0);
+    switch (direction) {
+        case "f":
+            horizontalPos += value;
+            break;
+        case "d":
+            depth += value;
+            break;
+        case "u":
+            depth -= value;
+    }
+});
+
+rl.on("close", () => console.log(depth * horizontalPos))
