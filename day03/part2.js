8ef0fef5567e212cbb62a69a70418f19fa60ec51
thejan14 <jan.oberpichler@googlemail.com>
2021-12-03 16:51:42 +0100

    Solution day 3 part 2

---
+++ day03/part2.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const fs = require("fs");
+
+const data = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .split("\r\n");
+
+const oxyGenRating = findRating(data, 0, (bitBalance) => bitBalance >= 0);
+const co2GenRating = findRating(data, 0, (bitBalance) => bitBalance < 0);
+
+console.log(oxyGenRating * co2GenRating);
+
+function findRating(currRange, bitPosition, bitCriteria) {
+  const bitBalance = getBitBalanceInPosition(currRange, bitPosition);
+  let newRange;
+  if (bitCriteria(bitBalance)) {
+    newRange = currRange.filter((l) => l.charAt(bitPosition) === "1");
+  } else {
+    newRange = currRange.filter((l) => l.charAt(bitPosition) === "0");
+  }
+
+  if (newRange.length === 1) {
+    return Number.parseInt(newRange[0], 2);
+  } else {
+    return findRating(newRange, bitPosition + 1, bitCriteria);
+  }
+}
+
+function getBitBalanceInPosition(range, bitPosition) {
+  return range.reduce(
+    (acc, line) => (acc += line.charAt(bitPosition) === "1" ? 1 : -1),
+    0
+  );
+}
