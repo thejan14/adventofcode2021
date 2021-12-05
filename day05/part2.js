a285f4e04a1cf4b716ea6af4071ddff48c1b8c76
thejan14 <jan.oberpichler@googlemail.com>
2021-12-05 12:40:31 +0100

    Solution day 5 part 2

---
+++ day05/part2.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const fs = require("fs");
+
+const data = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .split("\n")
+  .map((line) =>
+    line.split(" -> ").map((coord) => coord.split(",").map((n) => Number(n)))
+  );
+
+// count how often each coordinate is hit
+const coordinateOccurencesDict = {};
+for (const lineSegment of data) {
+  getVentCoordinates(lineSegment).forEach((coord) => {
+    if (coordinateOccurencesDict[coord]) {
+      coordinateOccurencesDict[coord] += 1;
+    } else {
+      coordinateOccurencesDict[coord] = 1;
+    }
+  });
+}
+
+const overlaps = Object.keys(coordinateOccurencesDict).filter(
+  (k) => coordinateOccurencesDict[k] > 1
+);
+
+console.log(overlaps.length);
+
+function getVentCoordinates([[x1, y1], [x2, y2]]) {
+  if (x1 === x2) {
+    const start = y1 < y2 ? y1 : y2;
+    const length = Math.abs(y1 - y2) + 1;
+    return Array.from({ length }, (_, i) => `${x1},${start + i}`);
+  } else if (y1 == y2) {
+    const start = x1 < x2 ? x1 : x2;
+    const length = Math.abs(x1 - x2) + 1;
+    return Array.from({ length }, (_, i) => `${start + i},${y1}`);
+  } else {
+    const length = Math.abs(x1 - x2) + 1;
+    const xChange = (x1 - x2) / Math.abs(x1 - x2);
+    const yChange = (y1 - y2) / Math.abs(y1 - y2);
+    return Array.from(
+      { length },
+      (_, i) => `${x1 - xChange * i},${y1 - yChange * i}`
+    );
+  }
+}
