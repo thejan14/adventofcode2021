cb02fa0f22579425afa2d36c1aa3bec3d07f5ad9
thejan14 <jan.oberpichler@googlemail.com>
2021-12-07 18:01:00 +0100

    Update solution day 7 part 2

--- day07/part2.js
+++ day07/part2.js
@@ -6,13 +6,25 @@ const crabPositions = fs
   .split(",")
   .map((n) => Number(n));
 
-const avg = crabPositions.reduce((a, b) => a + b) / crabPositions.length;
-const lowerAvg = Math.floor(avg);
+/*
+ * The solution initialy worked for me by just flooring the average,
+ * but it will not work for every case. See this awesome explanation:
+ * https://www.reddit.com/r/adventofcode/comments/rawxad/2021_day_7_part_2_i_wrote_a_paper_on_todays/
+ */
 
-const totalFuelCost = crabPositions
+const avg = crabPositions.reduce((a, b) => a + b) / crabPositions.length;
+const lowerAvg = Math.round(avg - 0.5);
+const higherAvg = Math.round(avg + 0.5);
+
+const totalFuelCostLower = crabPositions
   .map((n) => getFuelCost(Math.abs(lowerAvg - n)))
   .reduce((a, b) => a + b);
-console.log(totalFuelCost);
+
+const totalFuelCostHigher = crabPositions
+  .map((n) => getFuelCost(Math.abs(higherAvg - n)))
+  .reduce((a, b) => a + b);
+
+console.log(Math.min(totalFuelCostLower, totalFuelCostHigher));
 
 function getFuelCost(distance) {
   return (distance * (distance + 1)) / 2;
