1603fd1ebc43db886cfd7224b4d3484c77b3b628
thejan14 <jan.oberpichler@googlemail.com>
2021-12-09 21:00:06 +0100

    Implement script runner

---
+++ index.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const fs = require("fs");
+const cp = require("child_process");
+
+const dayFolderRegex = /day\d{2}/;
+const solutionScriptRegex = /part[12]\.js/;
+
+run();
+
+async function run() {
+  const solutions = fs
+    .readdirSync(__dirname, { withFileTypes: true })
+    .filter(
+      (directory) =>
+        directory.isDirectory() && dayFolderRegex.test(directory.name)
+    )
+    .flatMap((directory) =>
+      fs
+        .readdirSync(path.join(__dirname, directory.name), {
+          withFileTypes: true,
+        })
+        .filter(
+          (file) => !file.isDirectory() && solutionScriptRegex.test(file.name)
+        )
+        .map((file) => [directory.name, file.name])
+    );
+
+  for (const [dirName, fileName] of solutions) {
+    console.log(await runAndBenchmark(dirName, fileName));
+  }
+}
+
+function runAndBenchmark(dirName, fileName) {
+  return new Promise((resolve) => {
+    let start;
+    let output = "";
+    const solution = cp.fork(path.join(__dirname, dirName, fileName), {
+      stdio: "pipe",
+    });
+    solution.on("spawn", () => (start = performance.now()));
+    solution.on("close", () => {
+      const execTime = performance.now() - start;
+      resolve(
+        `${dirName}/${fileName} => ${output.trim()} (${execTime.toFixed(2)} ms)`
+      );
+    });
+    solution.stdout.on("data", (d) => (output += d));
+  });
+}
