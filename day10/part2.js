2798b57bf28eada5c1c29efef568e25d74b75f50
thejan14 <jan.oberpichler@googlemail.com>
2021-12-10 14:12:23 +0100

    Solution day 10 part 2

---
+++ day10/part2.js
@@ -0,0 +1,74 @@
+const path = require("path");
+const fs = require("fs");
+
+const subsystemCode = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .split("\n");
+
+let scores = [];
+for (const line of subsystemCode) {
+  const bracketStack = [];
+  let corrupted = false;
+  let i = 0;
+  while (i < line.length && !corrupted) {
+    const char = line.charAt(i);
+    i += 1;
+    if (isOpeningBracket(char)) {
+      bracketStack.push(getClosingBracket(char));
+    } else {
+      const counterBracket = bracketStack.pop();
+      if (char !== counterBracket) {
+        corrupted = true;
+      }
+    }
+  }
+
+  if (!corrupted && bracketStack.length) {
+    scores.push(
+      bracketStack
+        .reverse()
+        .reduce((score, char) => score * 5 + getCharacterScore(char), 0)
+    );
+  }
+}
+
+console.log(scores.sort((a, b) => a - b)[Math.floor(scores.length / 2)]);
+
+function isOpeningBracket(bracketChar) {
+  return (
+    bracketChar === "(" ||
+    bracketChar === "[" ||
+    bracketChar === "{" ||
+    bracketChar === "<"
+  );
+}
+
+function getClosingBracket(bracketChar) {
+  switch (bracketChar) {
+    case "(":
+      return ")";
+    case "[":
+      return "]";
+    case "{":
+      return "}";
+    case "<":
+      return ">";
+    default:
+      return "";
+  }
+}
+
+function getCharacterScore(completionChar) {
+  switch (completionChar) {
+    case ")":
+      return 1;
+    case "]":
+      return 2;
+    case "}":
+      return 3;
+    case ">":
+      return 4;
+    default:
+      return 0;
+  }
+}
