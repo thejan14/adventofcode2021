c23dbb17c373a444a861562b5ed2581cf72fb141
thejan14 <jan.oberpichler@googlemail.com>
2021-12-15 18:32:40 +0100

    implement A* algorithm for path finding

--- day15/part1.js
+++ day15/part1.js
@@ -18,27 +18,45 @@ const riskMap = input
   .split("\n")
   .map((line) => line.split("").map((n) => Number(n)));
 
-const frontier = [[0, 0]];
+let frontier = [{ pos: [0, 0], prio: riskMap[0][0] }];
+const enqueue = (pos, prio) => {
+  const index = frontier.findIndex((i) => i.prio > prio);
+  if (index !== -1) {
+    frontier.splice(index, 0, { pos, prio });
+  } else {
+    frontier.push({ pos, prio });
+  }
+};
+
 const costs = Array.from(Array(riskMap.length), () => Array(riskMap[0].length));
 costs[0][0] = 0;
 
-while (frontier.length > 0) {
-  const [x, y] = frontier.shift();
+const [targetX, targetY] = [riskMap.length - 1, riskMap.length - 1];
+let answer = 0;
+search: while (frontier.length > 0) {
+  const current = frontier.shift();
+  const [x, y] = current.pos;
   for (const [i, j] of directions) {
     const a = x + i;
     const b = y + j;
     if (riskMap[a] && riskMap[a][b]) {
-      const currRisk = costs[x][y] + riskMap[a][b];
-      const prevRisk = costs[a][b];
-      if (!prevRisk || currRisk < prevRisk) {
-        frontier.push([a, b]);
-        costs[a][b] = currRisk;
+      const nextRisk = riskMap[a][b];
+      const nextCosts = costs[x][y] + nextRisk;
+      const prevCosts = costs[a][b];
+      if (!prevCosts || nextCosts < prevCosts) {
+        if (a === targetX && b === targetY) {
+          answer = nextCosts;
+          break search;
+        } else {
+          const priority =
+            nextCosts + Math.abs(targetX - a) + Math.abs(targetY - b);
+          enqueue([a, b], priority);
+          costs[a][b] = nextCosts;
+        }
       }
     }
   }
 }
-
-const answer = costs[riskMap.length - 1][riskMap[0].length - 1];
 
 /* end solution */
 
